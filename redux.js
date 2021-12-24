105abca1824816486e34c3aea09f9fafe3f60e7a
c_kai <c_kai@ctrip.com>
2021-12-24 14:28:55 +0800

    update

--- redux.js
+++ redux.js
@@ -1,7 +1,29 @@
-const createStore = () => {
-
+const createStore = (reducer) => {
+  let state = {};
+  let listeners = []
+  const getState = () => {
+    return state;
+  }
+  const dispatch = (action) => {
+    state = reducer(action)
+    for(let i = 0;i<listeners.length;i++) {
+      const listener = listeners[i];
+      listener()
+    }
+  }
+  const subscribe = (callback) => {
+    listeners.push(callback)
+  }
+  const store = {
+    getState,
+    dispatch,
+    subscribe
+  }
+  return store;
 }
 
 const applyMiddleware = () => {
-  
+
 }
+
+https://segmentfault.com/a/1190000023084074
